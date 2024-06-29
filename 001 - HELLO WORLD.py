d5dfbedffe168c74bcb5083cf31cdce60edb6d80
William Galvão <Liam_draven@outlook.com>
2024-06-29 17:55:05 -0300

    O py ta on

---
+++ 001 - HELLO WORLD.py
@@ -0,0 +1,20 @@
+print('Olá, Mundo!')
+print('O Py ta on!')
+#Operações
+print(7 + 4)#soma
+print('7' + '4')#concatena
+print('7 + 4')#apresenta de forma literal
+#a virgula e o sinal de mais tem a memsma função, mas em alguns casos se torna melhor utilizar o +
+#print('Olá ', 5) nesse caso apresentaria erro ao rodar o código
+#print('Olá ' + 5)já neste caso ele sairia com "Olá 5"
+
+#Variáreis
+nome = 'William'
+idade = 28
+peso = 77.9
+print(nome,idade,peso)#Utilizando variáveis, usa-se a virgula
+
+seu_nome = input('Qual é seu nome?')
+sua_idade = input('Quantos anos você tem?')
+seu_peso = input('Qual o seu peso?')
+print(seu_nome,sua_idade,seu_peso)
