317fce55a1f928a9f019dc05fd380aa635ad08fe
William Galvão <Liam_draven@outlook.com>
2024-06-30 15:39:37 -0300

    python

--- 002 - ex000.py
+++ 002 - ex000.py
@@ -3,11 +3,9 @@ nome = input('Qual o seu nome? ')
 dia = 26
 mes = 'maio'
 ano = 1996
-print('Seja bem vindo,',nome,'!')
-print('você nasceu no dia',dia,'de',mes,'de',ano,', correto?')
+print('Seja bem vindo,', nome, '!')
+print('você nasceu no dia', dia, 'de', mes, 'de', ano, ', correto?')
 numero_1 = int(input('Digite um numero: '))
-numero_2 = int(input('digite outro numero: '))
+numero_2 = int(input('Digite outro numero: '))
 soma = numero_1 + numero_2
-print('A soma do número ',numero_1,' + ',numero_2,' é ',soma)
-
-
+print('A soma do número ', numero_1, ' + ', numero_2, ' é ', soma)
