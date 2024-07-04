0a035b670d59e13663ea2a0484b26e61050a8ddc
William Galvão <Liam_draven@outlook.com>
2024-07-04 17:19:34 -0300

    correções

--- Exercícios/007 - media.py
+++ Exercícios/007 - media.py
@@ -1,4 +1,4 @@
-nota1 = int(input('digite a primeira nota do aluno: '))
-nota2 = int(input('Digite a segunda nota do aluno: '))
+nota1 = float(input('digite a primeira nota do aluno: '))
+nota2 = float(input('Digite a segunda nota do aluno: '))
 media = (nota1 + nota2) / 2
 print(f'A média das notas foi de {media}')
--- Exercícios/010 - dolar.py
+++ Exercícios/010 - dolar.py
@@ -1,3 +1,4 @@
 real = float(input('Digite o quanto você possui, e descubra o quanto vale em dolar: R$ '))
 dolar: float = real / 5.60
-print(f'O seus R$ {real} valem US$ {dolar} ')
+print(f'O seus R$ {real} valem US$ {dolar:.2f} ')
+# A utilização do " :.'numero'f " serve para arrendondar as casas decimais após a vígula em um flutuante
