525d9fbc6a8694cf89feeded9eac9279a209b7fd
William Galvão <Liam_draven@outlook.com>
2024-07-08 15:05:33 -0300

    019

--- Exercícios/018 - Seno, cosseno , tangente.py
+++ Exercícios/018 - Seno, cosseno , tangente.py
@@ -8,6 +8,6 @@ cosseno = math.radians(math.cos(ang))
 # math.cos {serve para medir o cosseno de um ângulo}
 tangente = math.radians(math.tan(ang))
 # math.tan {serve para medir a tangente de um ângulo}
-print(f'O seno de {ang}° é {seno:.2f}')
-print(f'O cosseno de {ang}° é {cosseno:.2f}')
-print(f'A tangente de {ang}° é {tangente:.2f}')
+print(f'O seno de {ang}° é {seno}')
+print(f'O cosseno de {ang}° é {cosseno}')
+print(f'A tangente de {ang}° é {tangente}')
