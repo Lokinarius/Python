ec329d76234e64c1daf1ab3aef474d2030c35789
William Galvão <Liam_draven@outlook.com>
2024-07-09 16:18:29 -0300

    024

--- Exercícios/022- Nome completo.py
+++ Exercícios/022- Nome completo.py
@@ -1,7 +1,6 @@
 nome = str(input('Digite um nome completo: '))
 print(f'O nome com todas letras maiúsculas  fica: {nome.upper()}')
 print(f'O nome com todas letras minúsculas fica: {nome.lower()}')
-nome_sem_espacos = nome.replace(' ', '')
-print(f'Foi utilizado um total de {len(nome_sem_espacos)} letras')
+print(f'Foi utilizado um total de {len(nome) - nome.count(' ')} letras')
 primeiro_nome = nome.split()[0]
 print(f'O primeiro nome tem {len(primeiro_nome)} letras')
---
+++ Exercícios/024 - Santo.py
@@ -0,0 +1,3 @@
+city = input('Diga um nome de uma cidade: ').strip()
+result = ('A cidade começa com "Santo"' * city.startswith('Santo'))
+print(result)
