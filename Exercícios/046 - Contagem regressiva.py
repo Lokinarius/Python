0f9d8452115e4eb13ed7d4c6ee88c36f85b4177d
William Galvão <Liam_draven@outlook.com>
2024-07-20 23:57:53 -0300

    046

---
+++ Exercícios/046 - Contagem regressiva.py
@@ -0,0 +1,7 @@
+import time
+print('\t\tCONTAGEM REGRESSIVA')
+print('--------------------------------')
+for c in range(10,0,-1):
+    print(f'{c} segundos para o ano novo')
+    time.sleep(1)
+print('FELIZ ANO NOVO!!!')
