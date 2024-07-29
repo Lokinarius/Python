12dcfd1ac3710d73be431bd4695b2c26451a928e
William Galvão <Liam_draven@outlook.com>
2024-07-29 15:13:05 -0300

    Update 064 - Tratando vários valores.py

---
+++ Exercícios/064 - Tratando vários valores.py
@@ -0,0 +1,21 @@
+# Cabeçalho
+print('\t\tSOMA DE VALORES')
+print('---------------------------------')
+
+# Variaveis
+soma = 0
+n = 0
+
+# Entrada de dados
+while True:
+    print('\n================================')
+    num = float(input('Informe um número (ou 999 para sair): '))
+    if num == 999:
+        break
+    soma += num
+    n += 1
+
+
+# resultado da soma
+print('\n---------------------------------')
+print(f'foram digitados {n} valores, e a soma entre eles é {soma}')
