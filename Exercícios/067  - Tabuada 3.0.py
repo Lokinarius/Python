5140eeba57ddb7ff73d3ea0d288d26defb10dcbd
William Galvão <Liam_draven@outlook.com>
2024-08-01 17:32:12 -0300

    067

---
+++ Exercícios/067  - Tabuada 3.0.py
@@ -0,0 +1,17 @@
+# Cabeçalho
+print('\t\tTABUADA 3.0')
+
+# Entrada de dados
+while True:
+    print('================================\n')
+    num = int(input('Digite um número: '))
+    if num <= 0:
+        if num == 0:
+            print('O Número 0 digitado, o programa será encerrado...')
+        else:
+            print('Um Número negativo foi digitado, o programa será encerrado...')
+        break
+    else:
+        for n in range(1, 11):
+            mult = num * n
+            print(f'{num} X {n} = {mult}')
