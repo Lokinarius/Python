461f74d8a9af7b9f74cfff5a86217ff4c8d9e4c1
William Galvão <Liam_draven@outlook.com>
2024-08-12 17:43:13 -0300

    082

--- Exercícios/082 - Dividindo valores em várias listas.py
+++ Exercícios/082 - Dividindo valores em várias listas.py
@@ -1,2 +1,29 @@
 # Cabeçalho
-print('')
+print('\t\tDIVIDINDO VALORES EM VÁRIAS LISTAS')
+print('------------------------------------------------------')
+
+# Variaveis
+lista = []
+lista_pares = []
+lista_impares = []
+
+# Entrada de dados
+while True:
+    num = int(input('Digite um número: '))
+    lista.append(num)
+    if num % 2 == 0:
+        lista_pares.append(num)
+    else:
+        lista_impares.append(num)
+    resposta = input('Deseja continuar? (S/N): ').strip().upper()
+    while resposta not in ['S', 'N']:
+        print('Valor invalido! Digite S para continuar ou N para parar')
+        resposta = input('Deseja continuar? ').strip().upper()
+    if resposta == 'N':
+        break
+
+# Saída de dados
+print('-=' * 10)
+print(f'A lista completa é {lista}')
+print(f'A lista de números pares é {lista_pares}')
+print(f'A lista de números ímpares é {lista_impares}')
