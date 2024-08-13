c155e75a3fcc7743fddf82a9effee1bed2692ec5
William Galvão <Liam_draven@outlook.com>
2024-08-13 17:11:48 -0300

    Correções e aprimoramentos

--- Exercícios/081 - Extraindo dados de uma lista.py
+++ Exercícios/081 - Extraindo dados de uma lista.py
@@ -4,24 +4,24 @@ print('---------------------------------')
 
 # Variaveis
 lista = []
-numeros = 0
 
 # Entrada de dados
 while True:
     num = int(input('Digite um número: '))
     lista.append(num)
-    numeros += 1
     continua = input('Deseja continuar? ').strip().upper()
     while continua not in ['S', 'N']:
         print('Valor invalido! Digite S para continuar ou N para parar')
         continua = input('Deseja continuar? ').strip().upper()
     if continua == 'N':
         break
+# Ordenando a lista de forma decrescente
+lista.sort(reverse=True)
 
 # Saída de dados
 print('-=' * 10)
-print(f'Você digitou {numeros} elementos.')
-print(f'Os valores em ordem decrescente são {lista.sort(reverse=True)}')
+print(f'Você digitou {len(lista)} elementos.')
+print(f'Os valores em ordem decrescente são {lista}')
 if 5 in lista:
     print('O número 5 faz parte da lista!')
 else:
