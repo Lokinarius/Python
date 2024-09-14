a0138fa868d7c21d2fa81530effd3627a20d407f
William Galvão <Liam_draven@outlook.com>
2024-09-14 15:59:36 -0300

    Exercícios

---
+++ Exercícios/113 - Funções aprofundadas em Python.py
@@ -0,0 +1,5 @@
+"""
+113
+Reescreva a função leiaInt() que fizemos no desafio 104, incluindo agora a possibilidade da digitação de uma número
+de tipo inválido. Aproveite e cria também uma função leiaFloat() com a mesma funcionalidade.
+"""
---
+++ Exercícios/115a - Criando um menu em Python.py
@@ -0,0 +1,7 @@
+"""
+115a
+Crie um pequeno sistema modularizado que permite cadastrar pessoas pelo seu nome e idade em uma arquivo de texto
+simples.
+
+O sistema só vai ter 2 opções: cadastras uma nova pessoa e listar todas as pessoas cadastradas.
+"""
