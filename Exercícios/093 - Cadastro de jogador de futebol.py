e3555e7337763e69b4c30a6d119fc5b0ca9c4996
William Galvão <Liam_draven@outlook.com>
2024-09-22 00:11:00 -0300

    Update 093 - Cadastro de jogador de futebol.py

--- Exercícios/093 - Cadastro de jogador de futebol.py
+++ Exercícios/093 - Cadastro de jogador de futebol.py
@@ -6,3 +6,24 @@ Depois vai ler a quantidades de gols feitos em cada partida.
 No final final tudo isso será guardado em um dicionário,
 incluindo o total de gols feitos durante o campeonato.
 """
+# CABEÇALHO
+print("Cadastro de Jogador de Futebol")
+print("-="*30)
+
+
+
+# ENTRADA DE DADOS
+jogador = {
+    "nome": input("Nome do jogador: ").capitalize(),
+    "partidas" : int(input("Quantas partidas ele Jogou: ")),
+    "gols": []
+}
+# Adiciona gols feitos por partida
+for i in range(jogador["partidas"]):
+    gol = int(input(f"Quantos gols na partida {i+1}: "))
+    jogador["gols"].append(gol)
+
+# SAÍDA DE DADOS
+print("-="*30)
+for k, v in jogador.items():
+    print(f"{k.capitalize()}: {v}")
