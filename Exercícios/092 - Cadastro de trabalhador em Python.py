01d7699599b5ce6b90ab0d72c01406f97753797d
William Galvão <Liam_draven@outlook.com>
2024-09-21 23:39:29 -0300

    Update 092 - Cadastro de trabalhador em Python.py

--- Exercícios/092 - Cadastro de trabalhador em Python.py
+++ Exercícios/092 - Cadastro de trabalhador em Python.py
@@ -1,11 +1,11 @@
 """
-PARAMETROS
+092
 # Se a clt for dirente de zero ela para
 # Calculo da idade
 # idade da aposentadoria considerando 40 anos de contribuição
 """
 # Bibliotecas
-
+from datetime import datetime
 
 # Cabeçalho
 print('\t\tCadastro de trabalhador')
@@ -13,13 +13,20 @@ print('---------------------------------')
 
 # Entrada de dados
 trabalhador = {
-    'nome': input('Nome: '),
-    'nascimento': input('Ano de nascimento: '),
+    'nome': str(input('Nome: ')),
+    'nascimento': int(input('Ano de nascimento: ')),
+    'idade': datetime.now().year - int(input('Ana de nascimento: ')),
     'clt': input('CLT: '),
-    'contratação': input('Ano de contratação: '),
-    'salario' : input('Salário: ')
+    'contratação': int(input('Ano de contratação: ')),
 }
 
-
+# Se a clt for dirente de zero, para o calculo da idade e da aposentadoria
+if int(trabalhador['clt']) > 0:
+    trabalhador['contratação'] = int(input('Ano de contratação: '))
+    trabalhador['salário'] = float(input('Salário: R$'))
+    trabalhador['aposentadoria'] = trabalhador['idade'] + ((trabalhador['contratação'] + 35) - datetime.now().year)
 
 # Saida de dados
+print('-='*20)
+for k, v in trabalhador.items():
+    print(f'{k}: {v}')
