55d4dfff2b8031f24107763ef2a5ce8e9945b56f
William Galvão <Liam_draven@outlook.com>
2024-11-04 14:20:26 -0300

    Update 099 - Função que descobre o maior.py

--- Exercícios/099 - Função que descobre o maior.py
+++ Exercícios/099 - Função que descobre o maior.py
@@ -5,3 +5,13 @@ que receba vários parâmetros com valores inteiros.
 
 Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
 """
+
+def maior(primeiro, segundo):
+    if primeiro > segundo:
+        return primeiro
+    else:
+        return segundo
+
+primeiro = int(input("Digite o primeiro número: "))
+segundo = int(input("Digite o segundo número: "))
+print(maior(primeiro, segundo) + "É o maior número")
