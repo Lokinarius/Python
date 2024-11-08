90f6f64dc47a7962bee42e7b341cbe85476dc378
William Galvão <Liam_draven@outlook.com>
2024-11-08 17:35:08 -0300

    Update 099 - Função que descobre o maior.py

--- Exercícios/099 - Função que descobre o maior.py
+++ Exercícios/099 - Função que descobre o maior.py
@@ -1,17 +1,28 @@
-"""
-099
-Faça um programa que tenha uma função chamada maior(),
-que receba vários parâmetros com valores inteiros.
+#[IMPORTS]
+from time import sleep
 
-Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
-"""
+#[CABEÇALHO]
+print("Função que retorna o maior número")
+print("================================")
 
-def maior(primeiro, segundo):
-    if primeiro > segundo:
-        return primeiro
-    else:
-        return segundo
+#[ENTRADA DE DADOS]
+def maior(* numero):
+    contador = maior = 0
+    print('Analisando os valores passados..')
+    for valor in numero:
+        print(f'{valor}', end='')
+        sleep(0.5)
+        if contador == 0:
+            maior = valor
+        else:
+            if valor > maior:
+                maior = valor
+        contador += 1
+    print(f'Foram informados {contador} valores ao todo.')
+    print(f'O maior valor informado foi {maior}')
 
-primeiro = int(input("Digite o primeiro número: "))
-segundo = int(input("Digite o segundo número: "))
-print(maior(primeiro, segundo) + "É o maior número")
+
+#[PROGRAMA PRINCIPAL]
+maior(2,9,4,5,7,1)
+maior(4,7,0)
+maior(1,2)
