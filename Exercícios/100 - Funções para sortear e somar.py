6398a4294a0c99221d528cc46990aa837496e7b4
William Galvão <Liam_draven@outlook.com>
2024-11-09 17:48:20 -0300

    Update 100 - Funções para sortear e somar.py

--- Exercícios/100 - Funções para sortear e somar.py
+++ Exercícios/100 - Funções para sortear e somar.py
@@ -1,17 +1,27 @@
-"""
-100
-Faça um programa que tenha uma lista chamada números e duas funções chamadas sorteia() e somaPar().
-A primeira função vai sortear 5 números e vai colocá-los dentro da lista e a segunda função vai mostrar
-a soma entre todos os valores PARES sorteados pela função anterior.
-"""
-
+# [IMPORTS]
 import random
+from time import sleep
 
-def sorteia():
-    numeros = []
-    for _ in range(5):
-        numeros.append(random.randint(1, 100))
-    return numeros
+# [CABEÇALHO]
+print("Gerador de Números Randomicos")
+print("=" * 30)
 
-def somaPar(numeros):
-    return sum(num for num in numeros if num % 2 == 0)
+def sorteia(lista):
+    for contador in range(0, 5):
+        numero = random.randint(1, 100)
+        lista.append(numero)
+        print(f'{numero}', end='')
+        sleep(0.5)
+def somaPar(lista):
+    soma = 0
+    for numero in lista:
+        if numero % 2 == 0:
+            soma += numero
+    print(f'A soma dos valores pares é {soma}')
+
+# [Saída]
+numeros = list()
+sorteia(numeros)
+somaPar(numeros)
+
+
