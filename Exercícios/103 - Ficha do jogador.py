41c5b4f3b8938e28fbd04e2f0e9ce841beedd63d
William Galvão <Liam_draven@outlook.com>
2024-11-12 19:28:03 -0300

    Update 103 - Ficha do jogador.py

--- Exercícios/103 - Ficha do jogador.py
+++ Exercícios/103 - Ficha do jogador.py
@@ -6,3 +6,13 @@ opcionais: o nome de um jogador e quantos gols ele marcou.
 O programa deverá ser capaz de mostrar a ficha do jogador, mesmo que algum dado não tenha sido informado corretamente.
 
 """
+
+# CABEÇALHO
+print('Ficha do Jogador')
+print("-="*15)
+
+def ficha():
+nome = str(input('Nome do jogador: ')).capitalize()
+gols = int(input('Quantos gols ele marcou: '))
+
+
