4d6ac3f0d25b23a113be2cbe01b9f1f52665867f
William Galvão <Liam_draven@outlook.com>
2024-11-12 19:27:56 -0300

    Update 101 - Funções para votação.py

--- Exercícios/101 - Funções para votação.py
+++ Exercícios/101 - Funções para votação.py
@@ -1,6 +1,26 @@
-"""
-101
-Crie um programa que tenha uma fução chamada voto()
-que vai receber como parâmetro o ano de nascimento de uma pessoa, retornando um valor literal indicando se uma pessoa tem voto
-NEGADO, OPCIONAL ou OBRIGATÓRIO nas eleições.
-"""
+
+from datetime import datetime
+
+# Cabeçalho
+print("[URNA ELETRONICA]")
+print("=========================")
+
+# Função voto()
+def voto():
+    # Calculo de idade
+    ano_nascimento = int(input("Em que ano você nasceu? "))
+    ano_atual = datetime.now().year
+    idade = ano_atual - ano_nascimento
+    # Verificação de idade
+    if idade < 16:
+        return f"Você tem {idade} anos, você ainda não pode votar."
+    elif 18 <= idade <= 65:
+        return f"Você tem {idade} anos, seu voto é obrigatório."
+    else:
+        return f"Você tem {idade} anos, seu voto é opcional."
+
+# Saída
+print(voto())
+
+
+
