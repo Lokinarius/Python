69d57b84f4c704936db6a52f330bcf9ad76a5d2b
William Galvão <Liam_draven@outlook.com>
2024-11-13 19:42:44 -0300

    Update 103 - Ficha do jogador.py

--- Exercícios/103 - Ficha do jogador.py
+++ Exercícios/103 - Ficha do jogador.py
@@ -11,8 +11,20 @@ O programa deverá ser capaz de mostrar a ficha do jogador, mesmo que algum dado
 print('Ficha do Jogador')
 print("-="*15)
 
-def ficha():
+def ficha(nome='Desconhecido', gols=0):
+    print(f'O jogador {nome} fez {gols} gol(s).')
+
+# Chamada da função
 nome = str(input('Nome do jogador: ')).capitalize()
-gols = int(input('Quantos gols ele marcou: '))
+gols = input('Quantos gols ele marcou: ')
+
+# Verifica se o valor dos gols foi informado, caso contrário, assume 0
+if gols.isdigit():
+    gols = int(gols)
+else:
+    gols = 0
+
+# Chama a função com os valores informados
+ficha(nome, gols)
 
 
